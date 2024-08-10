825f69f5daa00a70ad736c2f74bcde04df07c38d
Lothbrock717 <91334582+Lothbrock717@users.noreply.github.com>
2024-08-11 04:04:36 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -59,7 +59,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙤𝙪𝙥](https://t.me/+kG9L8w7YAZsyMjE1).",
+            text="Something went Wrong. Contact my [𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝙂𝙧𝙪𝙥](https://t.me/+kG9L8w7YAZsyMjE1).",
             disable_web_page_preview=True
         )
         return 200
