71bad7d8caffe20287ec0214703b177c8aa928f6
Lothbrock717 <91334582+Lothbrock717@users.noreply.github.com>
2024-08-11 14:42:52 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -43,7 +43,7 @@ Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
 
 How to Use Bot & it's Benefits??
 
-📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
+📢 Sed me any File & It will be uploaded in My Database & You will Get the File Link.
 
 ⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
 """
